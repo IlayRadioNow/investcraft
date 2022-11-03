afbe8b8333c865c9400168ac09b4c5596950aa72
Радионов Илья <IlayRadioNow@yandex.ru>
2022-11-03 19:59:37 +0300

    Init

---
+++ src/main/java/ru/frusoft/investcraft/InvestcraftApplication.java
@@ -0,0 +1,13 @@
+package ru.frusoft.investcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestcraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/frusoft/investcraft/InvestcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.frusoft.investcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
